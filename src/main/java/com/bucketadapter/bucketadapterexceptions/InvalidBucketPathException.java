95ee8f8ad27464644704a58f566f24e586ccc2f6
T5uy0 <julienschneider964@gmail.com>
2026-03-07 08:46:21 +0100

    refactor: delete all file who work with bucket.

--- src/main/java/com/bucketadapter/bucketadapterexceptions/InvalidBucketPathException.java
+++
@@ -1,7 +0,0 @@
-package com.bucketadapter.bucketadapterexceptions;
-
-public class InvalidBucketPathException extends RuntimeException {
-  public InvalidBucketPathException(String message) {
-    super(message);
-  }
-}
